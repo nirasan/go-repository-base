a1afbb55f5d7677bd9b54094a01ef8673eb1b1cd
nirasan <niranirao@gmail.com>
2017-11-16 17:16:32 +0900

    add bench

---
+++ test/noreflect.go
@@ -0,0 +1,54 @@
+package test
+
+import (
+	"context"
+	"google.golang.org/appengine/datastore"
+)
+
+type User struct {
+	ID   int64 `repository:"id"`
+	Name string
+}
+
+type UserRepository struct {
+	ctx  context.Context
+	kind string
+}
+
+func NewUserRepository(ctx context.Context) *UserRepository {
+	return &UserRepository{ctx: ctx, kind: "User"}
+}
+
+func (r *UserRepository) Find(id int64) (*User, error) {
+	e := &User{}
+	if err := datastore.Get(r.ctx, r.NewKey(id), e); err != nil {
+		return nil, err
+	}
+	return e, nil
+}
+
+func (r *UserRepository) Create(e *User) error {
+	key := datastore.NewIncompleteKey(r.ctx, r.kind, nil)
+	newKey, err := datastore.Put(r.ctx, key, e)
+	if err != nil {
+		return err
+	}
+	e.ID = newKey.IntID()
+	_, err = datastore.Put(r.ctx, newKey, e)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *UserRepository) Delete(e *User) error {
+	err := datastore.Delete(r.ctx, r.NewKey(e.ID))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *UserRepository) NewKey(id int64) *datastore.Key {
+	return datastore.NewKey(r.ctx, r.kind, "", id, nil)
+}
---
+++ test/noreflect_test.go
@@ -0,0 +1,47 @@
+package test
+
+import (
+	"github.com/nirasan/go-repository-base"
+	"google.golang.org/appengine/aetest"
+	"testing"
+)
+
+func BenchmarkUserRepository_Find(b *testing.B) {
+	ctx, f, err := aetest.NewContext()
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer f()
+	r := NewUserRepository(ctx)
+
+	for i := 0; i < b.N; i++ {
+		e1 := &User{}
+		r.Create(e1)
+
+		e2, _ := r.Find(e1.ID)
+
+		r.Delete(e2)
+	}
+}
+
+func BenchmarkDatastoreRepository_Find(b *testing.B) {
+	ctx, f, err := aetest.NewContext()
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer f()
+
+	r, err := go_repository_base.NewDatastoreRepository(ctx, func() interface{} { return &User{} }, func() interface{} { return []*User{} })
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	for i := 0; i < b.N; i++ {
+		e1 := &User{}
+		r.Create(e1)
+
+		e2, _ := r.Find(e1.ID)
+
+		r.Delete(e2)
+	}
+}
